81382a4e2888a9fac090b248077fef8d1786715b
GarhettM <32fossil@gmail.com>
2020-03-18 20:39:00 -0700

    added the functions

--- content.js
+++ content.js
@@ -1,10 +1,10 @@
-var quantity = prompt("How much of this do you want to buy for $75:");
-var total = (75 * quantity);
-confirm(total);
-var receipt;
-if (total > 10000) {
-    confirm('Hey this is working');
-    receipt = 'CONGRATULATION! Youre probably going to be fine!'; 
+    var quantity = prompt("How much of this do you want to buy for $75:"); 
+    var total = (75 * quantity);
+    var receipt;
+    confirm('$' + total + '?');    
+    confirm("Thats it? Are you sure????");
+    if (total > 10000) {    
+    receipt = 'CONGRATULATIONS! Youre probably going to be fine!'; 
 }   else if (total > 8000) {
     receipt = 'Almost there but still not as much as the TP hoarders';
 }   else if (total > 5000) {
@@ -13,4 +13,8 @@ if (total > 10000) {
     receipt = 'Not even close! YOURE GOING TO DIE!'
 }
 
-document.write('<h1>' + receipt + '</h1>');
+    document.write('<h1>' + receipt + '</h1>');
+
+
+    var answer = prompt("Are you sick?")
+     
