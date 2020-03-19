361f840ee5c7536d9b1d689bf4a3e9bcf707bc2d
GarhettM <32fossil@gmail.com>
2020-03-19 13:44:56 -0700

    working on the java stuffs

--- content.js
+++ content.js
@@ -1,20 +1,27 @@
-    var quantity = prompt("How much of this do you want to buy for $75:"); 
-    var total = (75 * quantity);
-    var receipt;
-    confirm('$' + total + '?');    
-    confirm("Thats it? Are you sure????");
-    if (total > 10000) {    
-    receipt = 'CONGRATULATIONS! Youre probably going to be fine!'; 
-}   else if (total > 8000) {
-    receipt = 'Almost there but still not as much as the TP hoarders';
-}   else if (total > 5000) {
-    receipt = 'Come on you cheapskate! Think of the children!';
-}   else {
-    receipt = 'Not even close! YOURE GOING TO DIE!'
-}
+    
+    
+    function buystuff() {
+        var quantity = prompt("How much of this do you want to buy for $75:"); 
+        var total = (75 * quantity);
+        var receipt;
+        confirm('$' + total + '?');    
+        confirm("Thats it? Are you sure????");
+        if (total > 10000) {    
+        receipt = 'CONGRATULATIONS! Youre probably going to be fine!'; 
+    }   else if (total > 8000) {
+        receipt = 'Almost there but still not as much as the TP hoarders';
+    }   else if (total > 5000) {
+        receipt = 'Come on you cheapskate! Think of the children!';
+    }   else {
+        receipt = 'Not even close! YOURE GOING TO DIE!'
+    }
 
-    document.write('<h1>' + receipt + '</h1>');
+        return(receipt);
+    };
+    
+    
+    
+    
+  
+    
 
-
-    var answer = prompt("Are you sick?")
-     
---
+++ content2.js
@@ -0,0 +1,33 @@
+   
+   
+   
+   
+    
+    function ask()  {
+        
+        var sickness = prompt("Are you sick? yes or no?");
+        var diagnose1;
+        var function(sick())
+
+         if (sickness === yes)   {
+             return function sick();
+         }   
+            else if (sickness === no)   {
+            diagnose1 = 'Then why are you even here?!?!';
+        }
+        return(diagnose1);
+
+    }
+
+    function sick() {
+        var illness = prompt("How sick are you 1-10?");
+        var diagnose;
+
+        if (illness > 7)    {
+            diagnose = 'Damn! I hope you have insurance!';
+    }   else if (illness > 4)   {
+            diagnose = 'Gotcha! Well I got what you need!';
+    }   else   {
+            diagnose = 'Rub some dirt on it and drink water. NEXT!';
+    }
+     }   
