7f071399a4f382ffbfa5c7acfd2dbaf87cd2e51c
GarhettM <32fossil@gmail.com>
2020-03-19 18:09:05 -0700

    revised the java script in mybuis

--- content2.js
+++ content2.js
@@ -1,23 +1,23 @@
-   
-   
-   
-   
-    
+
+
+
     function ask()  {
         
-        var sickness = prompt("Are you sick? yes or no?");
-        var diagnose1;
-        var function(sick())
-
-         if (sickness === yes)   {
-             return function sick();
-         }   
-            else if (sickness === no)   {
-            diagnose1 = 'Then why are you even here?!?!';
-        }
-        return(diagnose1);
-
+        var areyou = prompt("Are you sick? yes or no?");
+        var speakBack;
+        
+        while(areyou === '') {
+            areyou = prompt("English M*********! Do you speak it!? Please enter yes or no!!!!");
     }
+
+        if (areyou === 'yes') {
+            speakBack = sick();
+    }   else  {
+            speakBack = 'Then what the hell are you doing here?!';
+    }    return speakBack;
+    }
+    
+
 
     function sick() {
         var illness = prompt("How sick are you 1-10?");
@@ -30,4 +30,33 @@
     }   else   {
             diagnose = 'Rub some dirt on it and drink water. NEXT!';
     }
-     }   
+        return diagnose;
+    }   
+
+
+
+    function buystuff() {
+        
+        var quantity = prompt("How much of this do you want to buy for $75:"); 
+        var total = (75 * quantity);
+        var receipt;
+        
+        for(var quantity = 0; quantity < 50; quantity + 50) {
+            confirm("you wanted me to add 50 to that right? Don't worry, I already did")
+        }
+
+        confirm('$' + total + '?');    
+        confirm("Thats it? Are you sure????");
+        
+        if (total > 10000) {    
+             receipt = 'CONGRATULATIONS! Youre probably going to be fine!'; 
+    }   else if (total > 8000) {
+             receipt = 'Almost there but still not as much as the TP hoarders';
+    }   else if (total > 5000) {
+             receipt = 'Come on you cheapskate! Think of the children!';
+    }   else {
+             receipt = 'Not even close! YOURE GOING TO DIE!'
+    }
+
+        return(receipt);
+    }
